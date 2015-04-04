5750654f42e5a29a67c2c377ad5be88a6f3fb23e
David Windell <david@outeredgeuk.com>
2015-04-05 00:17:52 +0100

    Add some same php.ini defaults

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,15 @@ MAINTAINER outer/edge <hello@outeredgeuk.com>
 COPY build.sh /build.sh
 RUN /build.sh
 
+COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY nginx-default /etc/nginx/conf.d/default
 COPY php-fpm.conf /usr/local/etc/php-fpm.conf
-COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+COPY php.ini /usr/local/etc/php/php.ini
 
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+ENV PHP_OPCACHE Off
 
 ONBUILD RUN composer self-update
 
--- build.sh
+++ build.sh
@@ -19,8 +19,11 @@ mkdir /tmp/ngx_pagespeed
 wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.tar.gz -O - | tar -zxf - --strip=1 -C /tmp/ngx_pagespeed
 wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz -O - | tar -zxf - -C /tmp/ngx_pagespeed
 
-# download and install nginx
+# install nginx
 mkdir /tmp/nginx
+mkdir -p /etc/nginx/conf.d
+mkdir -p -m 755 /var/cache/pagespeed
+chown -R www-data:www-data /var/cache/pagespeed
 wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O - | tar -zxf - -C /tmp/nginx --strip=1
 cd /tmp/nginx
 ./configure \
@@ -34,17 +37,16 @@ cd /tmp/nginx
     --add-module=/tmp/ngx_pagespeed
 make -j"$(nproc)"
 make install
-mkdir -p /etc/nginx/conf.d
-mkdir -p -m 755 /var/cache/pagespeed
-chown -R www-data:www-data /var/cache/pagespeed
 
 # download and install php
 apt-get install -y --no-install-recommends libcurl4-openssl-dev libreadline6-dev libmcrypt-dev libxml2-dev libpng-dev libicu-dev
 mkdir /tmp/php
+mkdir -p /usr/local/etc/php/conf.d
 wget http://php.net/get/php-$PHP_VERSION.tar.bz2/from/this/mirror -O - | tar -jxf - -C /tmp/php --strip=1
 cd /tmp/php
 ./configure \
     --with-config-file-path="/usr/local/etc/php" \
+    --with-config-file-scan-dir="/usr/local/etc/php/conf.d" \
     --with-libdir=/lib/x86_64-linux-gnu \
     --with-fpm-user=www-data \
     --with-fpm-group=www-data \
