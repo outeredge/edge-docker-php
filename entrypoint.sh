ba364e58796ec0b1b0ed6c7750399b460316378f
David Windell <david@outeredgeuk.com>
2015-05-22 01:45:41 +0100

    Add default config file for MSMTP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -e
 
-if [[ -e /etc/nginx/conf.d/default.conf.j2 ]]
-then
-    j2 /etc/nginx/conf.d/default.conf.j2 > /etc/nginx/conf.d/default.conf
-fi
-
-if [[ -e /etc/supervisor/conf.d/supervisord.conf.j2 ]]
-then
-    j2 /etc/supervisor/conf.d/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
-fi
+j2 /templates/nginx-default.conf.j2 > /etc/nginx/conf.d/default.conf
+j2 /templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
+j2 /templates/msmtprc.j2 > /etc/msmtprc
 
 exec "$@"
