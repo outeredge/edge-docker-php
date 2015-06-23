298ab9607b1e5b9c46eaa33c610c6331faccdbb6
David Windell <david@outeredgeuk.com>
2015-06-23 20:34:22 +0100

    Don't modify file permissions on run

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,5 @@ j2 /templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
 j2 /templates/msmtprc.j2 > /etc/msmtprc
 
 chown -R www-data:www-data /var/www
-chmod -R 775 /var/www
 
 exec "$@"
