c66276a81bac808a7f80914386f2ca56fbde41d6
David Windell <david@outeredgeuk.com>
2015-06-24 03:18:12 +0100

    Update umask

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ j2 /templates/nginx-default.conf.j2 > /etc/nginx/conf.d/default.conf
 j2 /templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
 j2 /templates/msmtprc.j2 > /etc/msmtprc
 
+umask 002
+
 chown -R www-data:www-data /var/www
 
 exec "$@"
