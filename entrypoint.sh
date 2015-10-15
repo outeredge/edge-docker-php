ac4bbc6c7780f8b8f29336a53958e458fc2a7810
David Windell <david@outeredgeuk.com>
2015-10-15 18:44:46 +0100

    Add PHP_MAX_CHILDREN

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 
 j2 /templates/nginx-default.conf.j2 > /etc/nginx/conf.d/default.conf
 j2 /templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
+j2 /templates/php-fpm.conf.j2 > /usr/local/etc/php-fpm.conf
 j2 /templates/msmtprc.j2 > /etc/msmtprc
 
 umask 002
 
-chown -R www-data:www-data /var/www
+chown -R www-data:www-data /var/cache/pagespeed /var/www
 chmod -R g=u /var/www
 
 exec "$@"
