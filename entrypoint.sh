8556ee7cea992e375544be2c71a5a188307bcc9c
David Windell <david@outeredgeuk.com>
2019-11-16 11:36:05 +0000

    Remove no longer used user id changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,6 @@ then
     echo "edge:$SSH_PASSWORD" | sudo chpasswd
 fi
 
-DOCKER_UID=`stat -c "%u" /var/www`
-DOCKER_GID=`stat -c "%g" /var/www`
-UID_EXISTS=`getent passwd $DOCKER_UID | cut -d: -f1`
-
-if [ -z "${UID_EXISTS}" ]
-then
-    sudo usermod -u $DOCKER_UID edge
-    sudo groupmod -g $DOCKER_GID edge
-    chown -R edge:edge /var/www /home/edge
-fi
-
 umask 002
 
 exec "$@"
