ae16adb401f351992ed266e0f12e99eda6a4c866
David Windell <david@outeredgeuk.com>
2019-12-12 00:38:58 +0000

    Revert "Add ability to user arbitary user on startup"
    This reverts commit 7a0d674643b906f307ed696cfa42a2d4bbc718a9.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -e
 
-CURRENT_UID="$(id -u)"
-CURRENT_GID="$(id -g)"
-
-if [ ${CURRENT_GID} -ne 1000 ]; then
-  if ! whoami &> /dev/null; then
-    echo "edge:x:${CURRENT_UID}:0:edge:/home/edge:/bin/bash" >> /etc/passwd
-  fi
-fi
-
 if [[ $ENABLE_SSH = "On" ]]
 then
     echo "edge:$SSH_PASSWORD" | sudo chpasswd
