61bd0f4d89132c1c3e7f4a73629415efc66fb942
David Windell <david@outeredgeuk.com>
2019-12-18 01:37:44 +0000

    Support running as arbitrary user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -e
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+     sed "s/^edge:x:1000:1000:/edge:x:$(id -u):1000:/" /etc/passwd > /tmp/passwd
+     cat /tmp/passwd > /etc/passwd
+     rm /tmp/passwd
+     sudo chown -R edge /home/edge /var/www
+  fi
+fi
+
+echo "PS1='\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" > /home/edge/.bashrc
+
 if [[ $ENABLE_SSH = "On" ]]
 then
     echo "edge:$SSH_PASSWORD" | sudo chpasswd
