a9f76fc503d355d1af7fff81241a8f5698edbd6c
David Windell <david@outeredgeuk.com>
2020-01-30 16:08:56 +0000

    Fix paths and enable ioncube by default

--- launch.sh
+++ launch.sh
@@ -6,8 +6,8 @@ sudo chmod -f 644 /etc/crontabs/*
 
 j2 /templates/nginx-default.conf.j2 | sudo dd status=none of=/etc/nginx/conf.d/default.conf
 j2 /templates/supervisord.conf.j2 | sudo dd status=none of=/etc/supervisord.conf
-j2 /templates/php-fpm.conf.j2 | sudo dd status=none of=/etc/php7/php-fpm.conf
-j2 /templates/xdebug.ini.j2 | sudo dd status=none of=/etc/php7/conf.d/xdebug.ini
+j2 /templates/php-fpm.conf.j2 | sudo dd status=none of=/etc/php5/php-fpm.conf
+j2 /templates/xdebug.ini.j2 | sudo dd status=none of=/etc/php5/conf.d/xdebug.ini
 j2 /templates/msmtprc.j2 | sudo dd status=none of=/etc/msmtprc
 
 sudo /usr/bin/supervisord
