90dbbf9b953dc902b4287f49adf8479bd7c7e78d
David Windell <david@outeredgeuk.com>
2020-02-04 18:43:12 +0000

    BC Add WEB_ROOT option

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ env | sudo dd status=none of=/etc/environment
 
 sudo chmod -f 644 /etc/crontabs/*
 
-j2 /templates/nginx-default.conf.j2 | sudo dd status=none of=/etc/nginx/conf.d/default.conf
+j2 /templates/nginx-default.conf.j2 | sudo dd status=none of=/etc/nginx/edge.conf
 j2 /templates/supervisord.conf.j2 | sudo dd status=none of=/etc/supervisord.conf
 j2 /templates/php-fpm.conf.j2 | sudo dd status=none of=/etc/php7/php-fpm.conf
 j2 /templates/xdebug.ini.j2 | sudo dd status=none of=/etc/php7/conf.d/xdebug.ini
