c581f1e393acd1c1a274f3421ab583ff97dbe91e
David Windell <david@outeredgeuk.com>
2020-02-05 16:07:56 +0000

    Parse nginx_conf through j2

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ env | sudo dd status=none of=/etc/environment
 sudo chmod -f 644 /etc/crontabs/*
 
 j2 /templates/nginx.conf.j2 | sudo dd status=none of=/etc/nginx/nginx.conf
-j2 /templates/nginx-default.conf.j2 | sudo dd status=none of=/etc/nginx/edge.conf
+j2 /templates/nginx-${NGINX_CONF}.conf.j2 | sudo dd status=none of=/etc/nginx/edge.conf
 j2 /templates/supervisord.conf.j2 | sudo dd status=none of=/etc/supervisord.conf
 j2 /templates/php-fpm.conf.j2 | sudo dd status=none of=/etc/php5/php-fpm.conf
 j2 /templates/xdebug.ini.j2 | sudo dd status=none of=/etc/php5/conf.d/xdebug.ini
