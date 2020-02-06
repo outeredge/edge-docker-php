c479a5af955cc9afd21f5b57debc48973e98627c
David Windell <david@outeredgeuk.com>
2020-02-06 11:22:37 +0000

    Fix nginx conf path

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ env | sudo dd status=none of=/etc/environment
 sudo chmod -f 644 /etc/crontabs/*
 
 j2 /templates/nginx.conf.j2 | sudo dd status=none of=/etc/nginx/nginx.conf
-j2 /templates/nginx-${NGINX_CONF}.conf.j2 | sudo dd status=none of=/etc/nginx/${NGINX_CONF}.conf
+j2 /templates/nginx-${NGINX_CONF}.conf.j2 | sudo dd status=none of=/etc/nginx/conf.d/${NGINX_CONF}.conf
 j2 /templates/supervisord.conf.j2 | sudo dd status=none of=/etc/supervisord.conf
 j2 /templates/php-fpm.conf.j2 | sudo dd status=none of=/etc/php7/php-fpm.conf
 j2 /templates/xdebug.ini.j2 | sudo dd status=none of=/etc/php7/conf.d/xdebug.ini
