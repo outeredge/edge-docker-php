ecbf664363c92ea7fab7dac13663ba6da4b35316
David Windell <david@outeredgeuk.com>
2022-07-06 14:54:45 +0000

    Update path and build scripts

--- publish.sh
+++ publish.sh
@@ -5,7 +5,11 @@ if [ -z "$1" ]; then
     exit
 fi;
 
+if [ ! -z "$DOCKER_PASS" ]; then
+    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+fi;
+
 echo "Building image outeredge/edge-docker-php:$1 with Dockerfile.php${1//./}"
-docker build --pull . -t outeredge/edge-docker-php:$1 -f Dockerfile.php${1//./}
-docker push outeredge/edge-docker-php:$1
+DOCKER_BUILDKIT=1 docker build --pull . -t outeredge/edge-docker-php:$1 -f Dockerfile.php${1//./} && \
+docker push outeredge/edge-docker-php:$1 && \
 echo "Complete!"
